53bc4c5561b414b3b58b690319861c07ad05a596
krishnakammari <63136494+krishnakammari@users.noreply.github.com>
2020-08-11 23:22:49 +0530

    Updates

---
+++ precache-manifest.130182b3ce922e84fbcd56caf7c21184.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c33eb4fb4673206d90b1c4b3e52a78f",
+    "url": "/fornt-end-webapp/index.html"
+  },
+  {
+    "revision": "42eda54d7b2f3bada99a",
+    "url": "/fornt-end-webapp/static/css/2.5ca9ed18.chunk.css"
+  },
+  {
+    "revision": "3f6d84ae13acfda954aa",
+    "url": "/fornt-end-webapp/static/css/main.12b14038.chunk.css"
+  },
+  {
+    "revision": "42eda54d7b2f3bada99a",
+    "url": "/fornt-end-webapp/static/js/2.c3181df8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fornt-end-webapp/static/js/2.c3181df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6d84ae13acfda954aa",
+    "url": "/fornt-end-webapp/static/js/main.605258b4.chunk.js"
+  },
+  {
+    "revision": "3587717386eaac05dea4",
+    "url": "/fornt-end-webapp/static/js/runtime-main.2ee8ad0a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fornt-end-webapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fornt-end-webapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fornt-end-webapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fornt-end-webapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fornt-end-webapp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
